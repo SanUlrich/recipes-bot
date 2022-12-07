ad8a0a6cad8afd8954106d2804303e827fcae96a
SanUlrich <SanUlrich@mail.ru>
2022-12-07 01:30:52 +0100

    add dotenv, hide my token

--- app/core.py
+++ app/core.py
@@ -1,8 +1,10 @@
+import os
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
 
-from app.config import TOKEN
 
+load_dotenv()
 
+TOKEN = os.environ.get('TOKEN')
 bot = Bot(token=TOKEN, parse_mode='html')
 dp = Dispatcher(bot)
-
